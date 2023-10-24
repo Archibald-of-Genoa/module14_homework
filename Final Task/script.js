df30be933f08db25fa4f96c2ded42fdd7a11fef0
Artem Chernov <archibaldofgenoa@yandex.ru>
2023-10-24 10:46:48 +0300

    Final Task: ready!

---
+++ Final Task/script.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const pageInput = document.getElementById("pageInput");
+    const limitInput = document.getElementById("limitInput");
+    const submitButton = document.getElementById("submitBtn");
+    const imageContainer = document.querySelector(".imageContainer");
+    const errorMessage = document.querySelector(".errorMessage");
+
+    const lastPage = localStorage.getItem("lastPage");
+    const lastLimit = localStorage.getItem("lastLimit");
+
+    if (lastPage && lastLimit) {
+        pageInput.value = lastPage;
+        limitInput.value = lastLimit;
+        fetchData(lastPage, lastLimit);
+    }
+
+    errorMessage.style.backgroundColor = "transparent";
+
+    submitButton.addEventListener("click", function () {
+        const page = parseInt(pageInput.value);
+        const limit = parseInt(limitInput.value);
+
+        const pageOutOfRange = page < 1 || page > 10;
+        const limitOutOfRange = limit < 1 || limit > 10;
+
+        if (isNaN(page) || pageOutOfRange) {
+            errorMessage.textContent = pageOutOfRange ? "Номер страницы вне диапазона от 1 до 10" : "Номер страницы должен быть числом";
+            errorMessage.style.backgroundColor = "white";
+            imageContainer.innerHTML = "";
+        } else if (isNaN(limit) || limitOutOfRange) {
+            errorMessage.textContent = limitOutOfRange ? "Лимит вне диапазона от 1 до 10" : "Лимит должен быть числом";
+            errorMessage.style.backgroundColor = "white";
+            imageContainer.innerHTML = "";
+        } else {
+            errorMessage.textContent = "";
+            errorMessage.style.backgroundColor = "transparent";
+            imageContainer.innerHTML = "";
+
+            fetchData(page, limit);
+
+            localStorage.setItem("lastPage", page);
+            localStorage.setItem("lastLimit", limit);
+        }
+    });
+
+    function fetchData(page, limit) {
+        const url = `https://picsum.photos/v2/list?page=${page}&limit=${limit}`;
+
+        fetch(url)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error("Ошибка HTTP: " + response.status);
+                }
+            })
+            .then((data) => {
+                data.forEach((item) => {
+                    const imgElement = document.createElement("img");
+                    imgElement.src = item.download_url;
+                    imgElement.alt = "Случайное изображение";
+                    imageContainer.appendChild(imgElement);
+                });
+            })
+            .catch((error) => {
+                console.error("Ошибка:", error);
+            });
+    }
+});
